4440223342515428f414aadeb74528e1edaf1f27
hoanglong040800 <hoanglong040800@gmail.com>
2024-02-14 11:51:39 +0700

    feat: setup prettier, eslint, husky, lint-staged

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const name = 'long';
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello {name}</Text>
     </View>
   );
 }
